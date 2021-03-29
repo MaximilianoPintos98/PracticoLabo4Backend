97147475892148d42851632cc9a634936ab64978
DanielSosa12 <sosa.danielb@gmail.com>
2021-03-29 10:06:52 -0300

    creacion de proyecto maven

---
+++ back-laboratorio/src/main/java/com/laboratorio/back/BackLaboratorioApplication.java
@@ -0,0 +1,13 @@
+package com.laboratorio.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackLaboratorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackLaboratorioApplication.class, args);
+	}
+
+}
