f7062945cff3e64962f269c7cd91843a708d9588
DanielSosa12 <sosa.danielb@gmail.com>
2021-03-30 00:36:02 -0300

    creacion de los metodos del service

--- back-laboratorio/src/test/java/com/laboratorio/back/services/EmpresaService.java
+++ back-laboratorio/src/test/java/com/laboratorio/back/services/EmpresaService.java
@@ -1,42 +1,83 @@
+
 package com.laboratorio.back.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.laboratorio.back.entities.Empresa;
+import com.laboratorio.back.repositories.EmpresaRepository;
 
 @Service
-public class EmpresaService implements BaseService<Empresa>{
+public class EmpresaService implements BaseService<Empresa> {
+
+	@Autowired
+	private EmpresaRepository empresaRepository;
 
 	@Override
+	@Transactional
 	public List<Empresa> findAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			List<Empresa> entities = empresaRepository.findAll();
+			return entities;
+ 		} catch (Exception e) {
+ 			throw new Exception(e.getMessage());
+		}
+
 	}
 
 	@Override
+	@Transactional
 	public Empresa findById(Long id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Optional<Empresa> entityOptional = empresaRepository.findById(id);
+			return entityOptional.get();
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
 	}
 
 	@Override
+	@Transactional
 	public Empresa save(Empresa entity) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			entity = empresaRepository.save(entity);
+			return entity;
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
 	}
 
 	@Override
+	@Transactional
 	public Empresa update(Long id, Empresa entity) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Optional<Empresa> entityOptional = empresaRepository.findById(id);
+			Empresa empresa = entityOptional.get();
+			empresa = empresaRepository.save(empresa);
+			return empresa; 
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
 	}
 
 	@Override
+	@Transactional
 	public boolean delete(Long id) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			if(empresaRepository.existsById(id)) {
+				empresaRepository.deleteById(id);
+				return true; 
+			} else {
+				throw new Exception();
+			}
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
 	}
 
 }
