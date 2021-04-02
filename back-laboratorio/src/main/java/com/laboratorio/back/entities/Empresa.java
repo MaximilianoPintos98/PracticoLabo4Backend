3eab4b37a9ac19f448d2ab61db1545d0e6d649f1
BrunoR96 <brunoricco96@gmail.com>
2021-04-02 10:23:40 -0300

    Relacion Empresa-Noticia

--- back-laboratorio/src/main/java/com/laboratorio/back/entities/Empresa.java
+++ back-laboratorio/src/main/java/com/laboratorio/back/entities/Empresa.java
@@ -1,6 +1,13 @@
 package com.laboratorio.back.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -24,5 +31,13 @@ public class Empresa extends Base{
 	private String info;
 	private Double latitud;
 	private Double longitud; 
+	
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinTable(
+			name = "Empresa_Noticias",
+			joinColumns = @JoinColumn(name = "empresa_id"),
+			inverseJoinColumns = @JoinColumn(name = "noticia_id")
+			)
+	private List<Noticias> noticia = new ArrayList<Noticias>();
 	
 }
