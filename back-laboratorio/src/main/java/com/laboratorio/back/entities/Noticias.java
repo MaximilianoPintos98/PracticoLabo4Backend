08c7e95c6a2867fe9cd4fff88adec5f03c6b2423
DanielSosa12 <sosa.danielb@gmail.com>
2021-04-02 15:20:13 -0300

    Modificaión en el atributo fecha tabla Noticias

--- back-laboratorio/src/main/java/com/laboratorio/back/entities/Noticias.java
+++ back-laboratorio/src/main/java/com/laboratorio/back/entities/Noticias.java
@@ -3,7 +3,10 @@ package com.laboratorio.back.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,6 +28,13 @@ public class Noticias extends Base{
 	private String imagen; 
 	private String contenidoHTML;
 	private char publicada;
+	
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date fecha; 
+	
+	@PrePersist
+	public void prePersist() {
+		this.fecha = new Date();
+	}
 
 }
