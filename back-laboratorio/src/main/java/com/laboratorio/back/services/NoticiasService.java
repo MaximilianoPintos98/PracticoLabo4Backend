40ddcf1da1043d229277574cc0d46892d4f32bff
DanielSosa12 <sosa.danielb@gmail.com>
2021-04-02 14:33:58 -0300

    Agregando la query de busqueda

--- back-laboratorio/src/main/java/com/laboratorio/back/services/NoticiasService.java
+++ back-laboratorio/src/main/java/com/laboratorio/back/services/NoticiasService.java
@@ -1,7 +1,10 @@
 package com.laboratorio.back.services;
 
+import java.util.List;
+
 import com.laboratorio.back.entities.Noticias;
 
 public interface NoticiasService extends BaseService<Noticias, Long>{
 
+	List<Noticias> search(String filtro) throws Exception;
 }
